4f313becbc22a666bb582cc30cb009eaf4a6af2b
johnnytham <76861466+johnnytham@users.noreply.github.com>
2021-01-10 16:26:08 +0800

    Update test.sql

--- Tables/test.sql
+++ Tables/test.sql
@@ -1,7 +1,7 @@
 Use CUSTOMERS;
-GO ;
+GO 
 CREATE TABLE Customer.test(id INT,name VARCHAR(20));
-GO ;
+GO 
 INSERT INTO Customer.test VALUES(1,'Tham');
 INSERT INTO Customer.test VALUES(2,'Arun');
 GO
