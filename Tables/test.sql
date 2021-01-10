75699980ce79cf3369a17f62872268ecdbf7e617
johnnytham <76861466+johnnytham@users.noreply.github.com>
2021-01-10 16:34:15 +0800

    Update test.sql

--- Tables/test.sql
+++ Tables/test.sql
@@ -3,7 +3,4 @@ GO
 CREATE TABLE Customer.test(id INT,name VARCHAR(20));
 GO
 TRUNCATE TABLE Customer.test
-GO 
-INSERT INTO Customer.test VALUES(1,'Tham');
-INSERT INTO Customer.test VALUES(2,'Arun');
 GO
