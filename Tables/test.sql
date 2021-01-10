ae8d3b8c7ce41e8cc61f3f8d652eb22f7ed95761
johnnytham <76861466+johnnytham@users.noreply.github.com>
2021-01-10 16:32:36 +0800

    Update test.sql

--- Tables/test.sql
+++ Tables/test.sql
@@ -1,6 +1,8 @@
 Use CUSTOMERS;
 GO 
 CREATE TABLE Customer.test(id INT,name VARCHAR(20));
+GO
+TRUNCATE TABLE Customer.test
 GO 
 INSERT INTO Customer.test VALUES(1,'Tham');
 INSERT INTO Customer.test VALUES(2,'Arun');
